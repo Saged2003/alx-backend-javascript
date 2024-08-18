b50d077f1a94170793d9e1f3e7c000418a61c4a1
Saged2003 <sagedryan775@gmail.com>
2024-08-18 16:00:04 -0700

    saged

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+
+export default class Currency {
+    constructor(code, name) {
+        this.name = name;
+        this.code = code;
+    }
+
+    set name(name) {
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set code(code) {
+        if (typeof code !== 'string') {
+            throw new TypeError('Code must be a String');
+        }
+        this._code = code;
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+}
+
